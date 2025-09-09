b1e99f61981cce43d763eb290fb6a40c2ee1f91e
Nandra29 <mohammad_shihab_34rpl@student.smktelkom-mlg.sch.id>
2025-09-09 09:54:54 +0700

    Add files via upload

---
+++ cartest.java
@@ -0,0 +1,11 @@
+public class cartest {
+    public static void main(String[] args) {
+        car car1 = new car();
+        car1.carInfo();  
+        System.out.println();
+        
+        car car2 = new car(2,true);
+        car2.carInfo();
+    }
+    
+}
